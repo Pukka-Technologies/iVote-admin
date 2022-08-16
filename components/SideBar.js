4c4687b30a4560ed309d1b37644491867978cee3
Baaba <baabadampare@gmail.com>
2022-08-16 19:20:54 +0000

    edit sidebar

--- components/SideBar.js
+++ components/SideBar.js
@@ -9,7 +9,7 @@ const SideBar = () => {
   return (
     <div className="flex flex-col justify-center font-text p-10">
       {/*logo*/}
-      <h1 className="font-bold text-[1.3em]">Media Billo</h1>
+      <h1 className="font-bold text-[1.3em] text-center">Media Billo</h1>
       <ul className="pt-[4em] gap-3 flex flex-col">
         <li className="flex gap-3 items-center bg-green-200 cursor-pointer w-[14em] py-[0.5em] rounded-xl pl-[1em]">
           <MdOutlineEmojiEvents />
