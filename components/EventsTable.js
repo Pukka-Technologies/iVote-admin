bb5540dee5ca76197e6e3bc4959cf57383140386
Baaba <baabadampare@gmail.com>
2022-08-17 16:59:13 +0000

    add utils events table

--- components/EventsTable.js
+++ components/EventsTable.js
@@ -1,77 +1,38 @@
 import React from "react";
 import { AiOutlineEye, AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
+import EventsItem from "../utils/eventsTable";
 
-const eventImg = "/assets/events.jpg";
+const EventItem = ({ eventImg, name, date, status }) => {
+  <article className="flex justify-between py-[1em] border-b-2 ">
+    <div className="flex gap-6">
+      <img src={eventImg} className="w-10 h-10 rounded-md" />
+      <div>
+        <h3 className="font-bold">{name}</h3>
+        <p className="text-xs text-gray-400">{date}</p>
+      </div>
+    </div>
+    <div className="text-gray-500">{status}</div>
+    <div className="flex gap-4 cursor-pointer">
+      <AiOutlineEye />
+      <AiOutlineEdit />
+      <AiOutlineDelete />
+    </div>
+  </article>;
+};
 
 const EventsTable = () => {
   return (
     <div className="bg-white font-text mx-[5em]  px-[2em] py-[1em] rounded-lg">
       <h1 className="border-b-2 font-bold text-lg">All Events</h1>
-      <article className="flex justify-between py-[1em] border-b-2 ">
-        <div className="flex gap-6">
-          <img src={eventImg} className="w-10 h-10 rounded-md" />
-          <div>
-            <h3 className="font-bold">Miss Agriculture</h3>
-            <p className="text-xs text-gray-400">14 April, 2022</p>
-          </div>
-        </div>
-        <div className="text-gray-500">Closed</div>
-        <div className="flex gap-4 cursor-pointer">
-          <AiOutlineEye />
-          <AiOutlineEdit />
-          <AiOutlineDelete />
-        </div>
-      </article>
-
-      <article className="flex justify-between py-[1em] border-b-2 ">
-        <div className="flex gap-6">
-          <img src={eventImg} className="w-10 h-10 rounded-md" />
-          <div>
-            <h3 className="font-bold">Miss Agriculture</h3>
-            <p className="text-xs text-gray-400">14 April, 2022</p>
-          </div>
-        </div>
-        <div className="text-gray-500">Closed</div>
-        <div className="flex gap-4 cursor-pointer">
-          <AiOutlineEye />
-          <AiOutlineEdit />
-          <AiOutlineDelete />
-        </div>
-      </article>
-
-      <article className="flex justify-between py-[1em] border-b-2 ">
-        <div className="flex gap-6">
-          <img src={eventImg} className="w-10 h-10 rounded-md" />
-          <div>
-            <h3 className="font-bold">Miss Agriculture</h3>
-            <p className="text-xs text-gray-400">14 April, 2022</p>
-          </div>
-        </div>
-        <div className="text-gray-500">Closed</div>
-        <div className="flex gap-4 cursor-pointer">
-          <AiOutlineEye />
-          <AiOutlineEdit />
-          <AiOutlineDelete />
-        </div>
-      </article>
-
-      <article className="flex justify-between py-[1em] border-b-2 ">
-        <div className="flex gap-6">
-          <img src={eventImg} className="w-10 h-10 rounded-md" />
-          <div>
-            <h3 className="font-bold">Miss Agriculture</h3>
-            <p className="text-xs text-gray-400">14 April, 2022</p>
-          </div>
-        </div>
-        <div className="text-gray-500">Closed</div>
-        <div className="flex gap-4 cursor-pointer">
-          <AiOutlineEye />
-          <AiOutlineEdit />
-          <AiOutlineDelete />
-        </div>
-      </article>
-
-      
+      {EventsItem.map((item, index) => (
+        <EventItem
+          eventImg={item.eventImg}
+          name={item.name}
+          date={item.date}
+          status={item.status}
+          key={index}
+        />
+      ))}
     </div>
   );
 };
