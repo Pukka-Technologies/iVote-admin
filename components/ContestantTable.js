9baa108bc7e7d839ad36cc7839927c5295ede0ff
Baaba <baabadampare@gmail.com>
2022-08-20 10:24:00 +0000

    start contestants table

---
+++ components/ContestantTable.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { AiOutlineEye, AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
+import Contestants from "../utils/contestantsTable"
+
+const EventItem = ({ eventImg, name, date, status }) => (
+  <article className="flex justify-between py-[1em] border-b-2 ">
+    <div className="flex gap-6 w-[20%]">
+      <img src={eventImg} className="w-10 h-10 rounded-md" />
+      <div>
+        <h3 className="font-bold">{name}</h3>
+        <p className="text-xs text-gray-400">{date}</p>
+      </div>
+    </div>
+    <div className="text-gray-500">{status}</div>
+    <div className="flex gap-4 cursor-pointer">
+      <AiOutlineEye />
+      <AiOutlineEdit />
+      <AiOutlineDelete />
+    </div>
+  </article>
+);
+
+const ContestantTable = () => {
+  return (
+    <div className="bg-white font-text mx-[5em]  px-[2em] py-[1em] rounded-lg">
+      <h1 className="border-b-2 font-bold text-lg pb-[0.8em]">All Events</h1>
+      {Contestants.map((item, index) => (
+        <EventItem
+          eventImg={item.eventImg}
+          name={item.name}
+          date={item.date}
+          status={item.status}
+          key={index}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default ContestantTable
---
+++ utils/contestantsTable.js
@@ -0,0 +1,26 @@
+const Contestants = [
+  {
+    name: "Miss Agriculture",
+    date: "14 April, 2022",
+    eventImg: "/assets/events.jpg",
+    status: "Open",
+  },
+  {
+    name: "Miss World",
+    date: "13 April, 2022",
+    eventImg: "/assets/events2.jpg",
+    status: "Open",
+  },
+  {
+    name: "Miss Universe",
+    date: "10 April, 2022",
+    eventImg: "/assets/events.jpg",
+    status: "Closed",
+  },
+  {
+    name: "Miss Malaika",
+    date: "18 April, 2022",
+    eventImg: "/assets/events2.jpg",
+    status: "Closed",
+  },
+];
