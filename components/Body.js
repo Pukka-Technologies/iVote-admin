74619d0faead12887d2820e68980b42a52a66fbe
Baaba <baabadampare@gmail.com>
2022-08-24 14:54:05 +0000

    edit events table margin

--- components/Body.js
+++ components/Body.js
@@ -7,7 +7,7 @@ const Body = () => {
   return (
     <>
       <Navbar />
-      <div className="px-[3.5em] bg-gray-100 min-h-screen overflow-x-hidden box-border">
+      <div className="px-[3.4em] bg-gray-100 min-h-screen overflow-x-hidden box-border">
         <Tabs />
         <EventsTable />
       </div>
