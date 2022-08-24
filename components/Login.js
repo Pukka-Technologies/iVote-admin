c3da6b74d620ab77e1ec1d9fef2eef5ef5b615b7
Baaba <baabadampare@gmail.com>
2022-08-24 14:14:23 +0000

    edit login page

--- components/Login.js
+++ components/Login.js
@@ -9,17 +9,18 @@ const Login = () => {
       <RiAdminLine className="text-9xl bg-green-400 rounded-full p-5 text-white mb-[0.4em]" />
       <form className="w-[22em] justify-center flex flex-col">
         <input
-          className="h-[3em] px-[1em] rounded-lg border-none focus:outline-green-400"
+          className="h-[3em] px-[1em] border-none focus:outline-green-400"
           placeholder="USERNAME"
         ></input>
         <br></br>
         <input
-          className="h-[3em] px-[1em] rounded-lg focus:outline-green-400"
+          type="password"
+          className="h-[3em] px-[1em] focus:outline-green-400"
           placeholder="PASSWORD"
         ></input>
         <br></br>
         <Link href="/main">
-          <button className="my-[0.5em] bg-green-400 px-[1em] py-[0.6em] rounded-lg text-white">
+          <button className="my-[0.5em] bg-green-400 px-[1em] py-[0.6em] text-white">
             LOGIN
           </button>
         </Link>
