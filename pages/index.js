9685ae7a91ab7668ecda109b3de15ea973c50045
qbentil <bentilshadrack72@gmail.com>
2022-08-25 17:54:06 +0000

    Make Dasbboard Custom Auth Handler

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Login from "../components/Login.js";
+import Main from "../components/Main/";
 
 
-
+import { useStateValue } from "../context/StateProvider.js";
 export default function Home() {
+  const [{ user }, dispatch] = useStateValue();
   return (
-    <Login/>
+    <>  
+      {!user ? <Login /> : <Main />}
+    </>
   );
 }
