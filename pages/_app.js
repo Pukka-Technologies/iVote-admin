1a311730021542e096d8cec9c00dceefb270ca60
qbentil <bentilshadrack72@gmail.com>
2022-08-25 17:30:28 +0000

    Configure App to use Global State Variables

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { StateProvider } from "../context/StateProvider";
+import { initialState } from "../context/initialState";
+import reducer from "../context/reducer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; //Global styles for toastify
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateProvider StateProvider initialState={initialState} reducer={reducer}>
+      <ToastContainer /> 
+      <Component {...pageProps} />
+    </StateProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
