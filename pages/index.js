197f13e4814f0ed8fe591c55de727ba80b3e4238
qbentil <bentilshadrack72@gmail.com>
2022-08-29 20:03:12 +0000

    Refresh user on start

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,25 @@
+import { GET_SESSION_USER } from "../utils/session.js";
 import Login from "../components/Login.js";
 import Main from "../components/Main/";
-
-
+import { useEffect } from "react";
 import { useStateValue } from "../context/StateProvider.js";
+
 export default function Home() {
   const [{ user }, dispatch] = useStateValue()
 
+useEffect(() => {
+  const fetchSession = async () => {
+      const session_user = await GET_SESSION_USER()
 
+    if(session_user){
+      dispatch({
+        type: "SET_USER",
+        user:session_user
+      })
+    }
+  };
+  fetchSession();
+}, [])
   if (!user)
   {
     return <Login />
