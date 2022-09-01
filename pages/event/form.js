70c465339425a2092df8c92c1925f570a2a48874
qbentil <bentilshadrack72@gmail.com>
2022-09-01 01:30:41 +0000

    Add event form

--- pages/event/form.js
+++ pages/event/form.js
@@ -1,85 +1,93 @@
-import React from "react";
-import { MdCloudUpload, MdOutlineDataSaverOn } from "react-icons/md";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { RangeDatePicker } from "../../components/Datepicker";
+import ImageUploader from "../../components/ImageUploader";
+import { ImSpinner3 } from "react-icons/im";
+const Form = () => {
+  const [image, setImage] = useState(null);
+  const [imageURI, setImageURI] = useState(null);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+  const [loading, setLoading] = useState(false);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!image) {
+      toast.error("Upload event photo");
+      return;
+    }
+    if (!name || !description) {
+      toast.error("Fill event name and description");
+      return;
+    }
+    if (!startDate || !endDate) {
+      toast.error("Select event opening and closing date");
+      return;
+    }
+    setLoading(true);
+    const formData = new FormData();
+    formData.append("image", image);
+    formData.append("name", name);
+    formData.append("description", description);
+    formData.append("startDate", startDate);
+    formData.append("endDate", endDate);
 
-const   Form = () => {
+    const eventData = {
+      image: imageURI,
+      name: name,
+      description: description,
+      startDate: startDate,
+      endDate: endDate,
+    };
+
+    console.log(eventData);
+    setLoading(false);
+  };
   return (
-    <div className="px-[3.4em] bg-gray-100 min-h-screen overflow-x-hidden box-border font-text">
-      <div className="min-h-screen overflow-x-hidden  my-3">
-        <div className=" min-h-screen overflow-x-hidden  box-border border-2 border-dotted pt-3 border-gray-300">
-          <div className="h-[26rem] overflow-x-hidden mx-3 box-border flex items-center justify-center border-2 border-dotted border-gray-300">
-            <label
-              htmlFor="file-upload"
-              className="flex flex-col justify-center items-center w-full h-full rounded-lg  cursor-pointer"
-            >
-              <div className="flex flex-col justify-center items-center pt-5 pb-6 gap-2">
-                <MdCloudUpload className="text-gray-500 text-3xl " />
-                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                  <span className="font-semibold">Click here to upload</span>
-                </p>
-                <p className="text-xs text-gray-500 dark:text-gray-400">
-                  PNG, JPG or GIF (MAX. 800x400px)
-                </p>
-              </div>
-              <input
-                id="file-upload"
-                name="uploadimage"
-                type="file"
-                accept="image/*"
-                className="hidden"
-                onChange={(e) => uploadImage(e)}
-              />
-            </label>
-          </div>
-          <div className="mt-10">
-            <div className="flex flex-row items-center  justify-start mx-3 mt-5  gap-3 pb-2">
-              <div className="flex flex-row items-center  justify-start mt-5 bg-white  w-full gap-3   border-none ">
-                <input
-                  type="text"
-                  placeholder="NAME OF EVENT"
-                  className="text-gray-500  bg-transparent pl-2   h-[3em]  focus:outline-green-400   w-full outline-none"
-                />
-              </div>
-
-              <div className="flex flex-row items-center  justify-start mt-5 bg-white  w-full gap-3   border-none ">
-                <input
-                  type="text"
-                  placeholder="EVENT DESCRIPTION"
-                  className="text-gray-500  bg-transparent pl-2   h-[3em]  focus:outline-green-400   w-full outline-none"
-                />
-              </div>
-            </div>
-            <div className="flex flex-row items-center  justify-start mx-3 mt-5  gap-3 pb-2">
-              <div className="flex flex-row items-center  justify-start mt-5 bg-white  w-full gap-3   border-none ">
-                <input
-                  type="date"
-                  placeholder="Open Date"
-                  className="text-gray-500  bg-transparent pl-2   h-[3em]  focus:outline-green-400   w-full outline-none"
-                />
-              </div>
-
-              <div className="flex flex-row items-center  justify-start mt-5 bg-white  w-full gap-3   border-none ">
-                <input
-                  type="date"
-                  placeholder="Close Date"
-                  className="text-gray-500  bg-transparent pl-2   h-[3em]  focus:outline-green-400   w-full outline-none"
-                />
-              </div>
-            </div>
-    
-           
-          </div>
-          <div className="flex flex-row items-center my-3 mx-3 justify-end">
-            <li className=" bg-green-400 px-[1em] py-[0.6em] flex  items-center justify-center gap-2  flex-row list-none">
-              <button className="font-medium text-black cursor-pointer ">
-                SAVE
-              </button>
-              <MdOutlineDataSaverOn />
-            </li>
-          </div>
-        </div>
+    <form
+      encType="multipart/form-data"
+      className="min-h-[86vh] px-2 bg-gray-100  overflow-x-hidden flex items-center justify-center gap-x-4"
+    >
+      <div className="w-[55%] h-72  overflow-x-hidden mx-3 box-border flex items-center justify-center border-2 border-dotted border-gray-300">
+        <ImageUploader
+          image={image}
+          setImage={setImage}
+          setImageURI={setImageURI}
+          className="h-full w-full"
+        />
       </div>
-    </div>
+      <div className="w-[45%] flex flex-col gap-y-3">
+        <input
+          type="text"
+          placeholder="Event Name"
+          className="bg-white px-2 py-3 focus:border-none  focus:outline-green-400 w-full"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <textarea
+          className="bg-white px-2 py-3 focus:border-none  focus:outline-green-400 w-full"
+          placeholder="Event Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+        <RangeDatePicker
+          startDate={startDate}
+          setStartDate={setStartDate}
+          endDate={endDate}
+          setEndDate={setEndDate}
+        />
+        <button
+          disabled={loading}
+          onClick={handleSubmit}
+          className="font-medium text-black cursor-pointer bg-green-400 px-[1em] py-[0.6em] flex  items-center justify-center gap-2"
+        >
+          {loading && <ImSpinner3 className="animate-spin" />}
+          {loading ? "Saving...." : "Save"}
+        </button>
+      </div>
+    </form>
   );
 };
 
