55c6df151f5ccd23563a9a66d167b41d2be73ace
qbentil <bentilshadrack72@gmail.com>
2022-09-01 12:55:05 +0000

    Creating event

--- pages/event/index.js
+++ pages/event/index.js
@@ -1,10 +1,27 @@
-import React from "react";
+import { useEffect } from "react";
 import  Form  from "./form";
 import SideBar from "../../components/SideBar";
 import Navbar from "../../components/Navbar";
+import { useStateValue } from "../../context/StateProvider";
+import { GET_SESSION_USER } from "../../utils/session";
 
 
 const Event = () => {
+  const [{ user }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    const fetchSession = async () => {
+      const session_user = await GET_SESSION_USER();
+      if (session_user) {
+        dispatch({
+          type: "SET_USER",
+          user: session_user,
+        });
+      }
+    };
+    fetchSession();
+    // console.log(user);
+  }, []);
   return (
     <div className="flex h-screen">
       <div>
