c961340cf71e3605ab4f2dec18bd305b8535d120
qbentil <bentilshadrack72@gmail.com>
2022-09-03 02:12:43 +0000

    Use live API

--- utils/axios.js
+++ utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8800/",
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8800/",
     withCredentials: true
 })
