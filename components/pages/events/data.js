6103ce57664896d5ef27a851d3cb40d0e25f4756
ksowah <sowahkelvin640@gmail.com>
2022-09-03 12:00:47 +0000

    show data on client

--- components/pages/events/data.js
+++ components/pages/events/data.js
@@ -1,19 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import React, { useEffect } from "react";
 import { AiOutlineEye, AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
+import { useStateValue } from "../../../context/StateProvider";
 import EventsItem from "../../../utils/eventsTable";
 
 const EventItem = ({ eventImg, name, date, status }) => (
   <article className="flex justify-between items-center py-[1em] border-b-2 ">
-    <div className="flex items-center gap-6 w-[20%]">
-      <img src={eventImg} className="w-14 h-14 rounded-md" alt="image" />
-      <div>
+    <div className="flex items-center gap-6 flex-1">
+      <img src={eventImg} className="w-24 h-16 rounded-md" alt="image" />
+      <div className="w-full">
         <h3 className="font-bold">{name}</h3>
         <p className="text-xs text-gray-400">{date}</p>
       </div>
     </div>
-    <div className="text-gray-500">{status}</div>
-    <div className="flex gap-4 cursor-pointer">
+    <div className="text-gray-500 flex flex-1 items-center justify-center">{status}</div>
+    <div className="flex gap-4 cursor-pointer flex-1 items-end justify-end">
       <AiOutlineEye className="hover:text-gray-600 hover:scale-125" />
       <AiOutlineEdit className="hover:text-gray-600 hover:scale-125" />
       <AiOutlineDelete className="hover:text-gray-600 hover:scale-125" />
@@ -22,15 +23,24 @@ const EventItem = ({ eventImg, name, date, status }) => (
 );
 
 const EventsList = () => {
+
+  const [{events}, dispatch] = useStateValue()
+
+  
+  // stat <today > end == opened
+  //  start > today
+
+  // const
+
   return (
     <div className="flex flex-col justify-center bg-white font-text mb-4 mt-6 px-[2em] py-[1em] rounded-lg">
       <h1 className="border-b-2 font-bold text-lg pb-[0.8em]">All Events</h1>
-      {EventsItem.map((item, index) => (
+      {events && events.map((item, index) => (
         <EventItem
-          eventImg={item.eventImg}
+          eventImg={item.imageURL}
           name={item.name}
-          date={item.date}
-          status={item.status}
+          date={item.open_date}
+          status={0}
           key={index}
         />
       ))}
