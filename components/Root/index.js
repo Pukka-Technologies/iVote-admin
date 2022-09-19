95f215b8f3fad30ad417bf30ac36d48b5255913c
ksowah <sowahkelvin640@gmail.com>
2022-09-19 18:16:32 +0000

    remove unused import

--- components/Root/index.js
+++ components/Root/index.js
@@ -1,13 +1,15 @@
 import { Dashboard } from "../pages";
 import Navbar from "../Navbar";
-import React from "react";
 import SideBar from "../sidebar";
 import { useState } from "react";
 
 const Root = () => {
   const [page, setPage] = useState("Dashboard");
   const [component, setComponent] = useState(<Dashboard />);
+
   return (
+    <>
+
     <div className="flex h-screen overflow-hidden ">
       <SideBar page={page} setComponent={setComponent} setPage={setPage} />
       <div className="w-[80%] flex flex-col">
@@ -17,6 +19,7 @@ const Root = () => {
         </div>
       </div>
     </div>
+    </>
   );
 };
 
